2a134377b1ecb10136a5cdedb59992f501a4a633
bumyy32 <bumyy32@gmail.com>
2021-12-06 21:44:18 -0300

    adicionado uns botao e comandos
    só isso msm

---
+++ else-if/Site-Notas-2/js/index.js
@@ -0,0 +1,9 @@
+var botao = document.querySelector("#btn-resultado");
+
+botao.addEventListener('click',function (aluno, nota1, nota2, nota3, nota4) {
+    var aluno = document.querySelector('#aluno').value
+    var nota1 = parseInt(document.querySelector('#nota1').value);
+    var nota2 = parseInt(document.querySelector('#nota2').value);
+    var nota3 = parseInt(document.querySelector('#nota3').value);
+    var nota4 = parseInt(document.querySelector('#nota4').value);
+})
